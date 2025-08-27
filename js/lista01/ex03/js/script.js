a271402bbb952d34de77d0a36672953f4940c557
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-08-27 19:22:05 -0300

    Update script.js

--- js/lista01/ex03/js/script.js
+++ js/lista01/ex03/js/script.js
@@ -1,16 +1,11 @@
-// 1. Solicita o nome completo do usuário
 let nome = prompt("Digite seu nome completo:");
-
-// 2. Solicita a idade e converte para um número inteiro
 let idade = parseInt(prompt("Digite sua idade:"));
 
-// 3. Verifica se a idade é suficiente para tirar a carteira
 if (idade >= 18) {
-  // Se a idade for 18 ou mais, exibe a mensagem de posse de carteira
   alert(`${nome}, você já POSSUI idade para tirar carteira`);
+
 } else {
-  // Se a idade for menor que 18, calcula os anos que faltam
   let anosFaltantes = 18 - idade;
-  // Exibe a mensagem de que a pessoa ainda não tem idade
   alert(`${nome}, você ainda NÃO POSSUI idade para tirar carteira, ainda falta(m) ${anosFaltantes} anos`);
+
 }

0170bc09aa046c72cedc5d0c0016eb65af74e552
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-08-27 19:25:53 -0300

    Update script.js

--- js/lista01/ex07/js/script.js
+++ js/lista01/ex07/js/script.js
@@ -1,5 +1,6 @@
 let r = parseInt(prompt("Digite o valor de R (0-255):"));
-    let g = parseInt(prompt("Digite o valor de G (0-255):"));
-    let b = parseInt(prompt("Digite o valor de B (0-255):"));
+let g = parseInt(prompt("Digite o valor de G (0-255):"));
+let b = parseInt(prompt("Digite o valor de B (0-255):"));
+
 
     document.write("<p style='color: rgb(" + r + "," + g + "," + b + ");'>Fundamentos de Web Design II</p>");

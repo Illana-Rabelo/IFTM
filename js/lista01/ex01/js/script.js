727635d26afcf51e65a096ac9bc1c116511bb0c0
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-08-27 13:03:55 -0300

    Add files via upload

---
+++ js/lista01/ex01/js/script.js
@@ -0,0 +1,11 @@
+// 1. Solicita o primeiro nome do usuário e armazena na variável 'primeiroNome'
+primeiroNome = prompt("Digite seu primeiro nome:");
+
+// 2. Solicita o sobrenome do usuário e armazena na variável 'sobrenome'
+sobrenome = prompt("Digite seu sobrenome:");
+
+// 3. Combina o primeiro nome e o sobrenome com um espaço no meio
+nomeCompleto = primeiroNome + " " + sobrenome;
+
+// 4. Exibe o nome completo em uma caixa de diálogo
+alert(nomeCompleto);

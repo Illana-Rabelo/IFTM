1d45c2c8b40bc5f8dcf03e905bc92f489f85d975
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-08-27 14:42:23 -0300

    Add files via upload

---
+++ js/lista01/ex10/js/script.js
@@ -0,0 +1,7 @@
+let x = parseInt(prompt("Digite um número inteiro positivo:"));
+
+let nome = prompt("Digite seu nome completo:");
+
+for (let i = 0; i < x; i++) {
+    document.write(nome + "<br>");
+}

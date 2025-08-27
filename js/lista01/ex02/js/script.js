9bcdce69f44bcb2c5a6be91dc08cef5030c2c8e8
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-08-27 19:21:05 -0300

    Update script.js

--- js/lista01/ex02/js/script.js
+++ js/lista01/ex02/js/script.js
@@ -1,5 +1,3 @@
-// Exibe a caixa de diálogo com a mensagem solicitada
 alert("Pressione o botão \"Ok\" para visualizar o nome da disciplina");
 
-// Depois que o usuário clica em OK, o nome da disciplina é escrito na página
 document.write("Fundamentos de Web Design II");

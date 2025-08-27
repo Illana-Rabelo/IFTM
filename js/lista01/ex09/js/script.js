6b3633fcaefb25bd0aec43f6f60624bab30ff23e
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-08-27 14:40:12 -0300

    Add files via upload

---
+++ js/lista01/ex09/js/script.js
@@ -0,0 +1,5 @@
+let expressao = prompt("Digite uma expressão matemática (ex: (5*8)-9):");
+
+let resultado = eval(expressao); // eval avalia a expressão
+
+alert("O resultado da expressão é: " + resultado);

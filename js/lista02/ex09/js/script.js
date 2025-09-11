f3c40decce87d022f86f262b8c7b54b6f4aae37c
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-09-11 20:09:24 -0300

    Add files via upload

---
+++ js/lista02/ex09/js/script.js
@@ -0,0 +1,10 @@
+let qtd = parseInt(prompt("Quantas pessoas irão apresentar?"));
+let nomes = [];
+for (let i = 0; i < qtd; i++) {
+  nomes.push(prompt("Nome da pessoa " + (i+1) + ":"));
+}
+nomes.sort(() => Math.random() - 0.5);
+document.write("<h3>Ordem de apresentação:</h3>");
+for (let i = 0; i < nomes.length; i++) {
+  document.write((i+1) + "º - " + nomes[i] + "<br>");
+}

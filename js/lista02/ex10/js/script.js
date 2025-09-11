1f52de6527422aeb6edb6acc3a16f7a3556b7bbf
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-09-11 20:28:28 -0300

    Update script.js

--- js/lista02/ex10/js/script.js
+++ js/lista02/ex10/js/script.js
@@ -1,6 +1,4 @@
 let totalCartas = 52;
 let carta = Math.floor(Math.random() * totalCartas) + 1;
-// O JS monta o caminho até a pasta das cartas
-
 
 document.write(`<img src="img/carta${carta}.png" alt="Carta sorteada">`);

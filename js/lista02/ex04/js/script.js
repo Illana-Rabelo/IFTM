05313cf7083fb8e01e51080d0dfde3c7aeee20dd
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-09-11 21:11:00 -0300

    Update script.js

--- js/lista02/ex04/js/script.js
+++ js/lista02/ex04/js/script.js
@@ -1,5 +1,7 @@
 let n = parseInt(prompt("Digite o valor de N:"));
 let m = parseInt(prompt("Digite o valor de M:"));
+
 if (n > m) [n, m] = [m, n];
 let numero = Math.floor(Math.random() * (m - n + 1)) + n;
+
 document.write(`Número sorteado entre ${n} e ${m}: ` + numero);

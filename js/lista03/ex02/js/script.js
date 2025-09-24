d230d8a1f41b923d35c5720bc0bb837a56dc2e28
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-09-23 21:21:28 -0300

    Add files via upload

---
+++ js/lista03/ex02/js/script.js
@@ -0,0 +1,8 @@
+btn = document.getElementById("btnExibir");
+texto = document.getElementById("txtNome");
+
+btn.addEventListener("click", exibirNome);
+
+function exibirNome(){
+    alert(texto.value.toUpperCase());
+}

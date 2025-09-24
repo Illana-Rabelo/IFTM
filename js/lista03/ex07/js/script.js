acf859f413f5d35eee0f2653ca00514d7d5c2ee9
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-09-24 19:55:57 -0300

    Update script.js

--- js/lista03/ex07/js/script.js
+++ js/lista03/ex07/js/script.js
@@ -1,19 +1,17 @@
-// Obtém os elementos do HTML
 const loginInput = document.getElementById('login');
 const senhaInput = document.getElementById('senha');
 const confirmaSenhaInput = document.getElementById('confirmaSenha');
 const btnEntrar = document.getElementById('btnEntrar');
 const btnLimpar = document.getElementById('btnLimpar');
 
-// Evento para o botão "Entrar"
 btnEntrar.addEventListener('click', () => {
-    // Primeira validação: verifica se o login está vazio
+    // verifica se o login está vazio
     if (loginInput.value === '') {
         alert("Por favor, digite o seu login.");
         return;
     }
 
-    // Segunda validação: verifica se as senhas são iguais
+    // verifica se as senhas são iguais
     if (senhaInput.value !== confirmaSenhaInput.value) {
         alert("As senhas digitadas não são iguais. Por favor, tente novamente.");
         senhaInput.value = '';
@@ -21,13 +19,12 @@ btnEntrar.addEventListener('click', () => {
         return;
     }
 
-    // Se todas as validações passarem
     alert("Todos os campos foram preenchidos corretamente.");
 });
 
-// Evento para o botão "Limpar"
 btnLimpar.addEventListener('click', () => {
     loginInput.value = '';
     senhaInput.value = '';
     confirmaSenhaInput.value = '';
+
 });

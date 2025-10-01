4f8107fc7cc72e3967dd640cd28098c917bcc439
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2025-10-01 19:59:37 -0300

    Update script.js

--- js/lista05/ex01/js/script.js
+++ js/lista05/ex01/js/script.js
@@ -1,41 +1,44 @@
-// elementos e estados do personagem
 const personagem = document.getElementById('personagem');
-const mensagemP = document.getElementById('mensagem');
+const mensagem = document.getElementById('mensagem');
 
-// estado inicial do personagem: Pensativo
-function setPensativo() {
-    personagem.src = 'imgs/Pensativo.png';
-    personagem.alt = 'Personagem Pensativo';
-    mensagemP.textContent = 'zzzzzzzzz!';
+function setEstado(estado, texto, animacaoClasse = null) {
+    // remove todas as classes de animação antes de aplicar a nova
+    personagem.classList.remove('imagem-nervoso', 'imagem-alegre');
+    personagem.src = `img/${estado}.png`;
+
+    mensagem.textContent = texto;
+
+    if (animacaoClasse) {
+        personagem.classList.add(animacaoClasse);
+    }
 }
 
-setPensativo(); 
 
-// ao posicionar o cursor do mouse sobre a imagem do personagem ele muda para Assustado
-personagem.addEventListener('mouseover', () => {
-    personagem.src = 'imgs/Assustado.png';
-    personagem.alt = 'Personagem Assustado';
-    mensagemP.textContent = 'O que você quer?'; // [cite: 31, 32]
-});
+function estadoPensativo() {
+    setEstado('pensativo', 'zzzzzzzzz!');
+}
 
-// ao retirar o cursor do mouse ele volta ao estado inicial
-personagem.addEventListener('mouseout', () => {
-    setPensativo(); 
-});
 
-personagem.addEventListener('click', () => {
-    const nomeUsuario = prompt("Por favor, digite o seu nome:");
-    
-    // cenário 1: Usuário não digitou nada, o personagem muda para nervoso
-    if (nomeUsuario === null || nomeUsuario.trim() === '') {
-        personagem.src = 'imgs/Nervoso.png';
-        personagem.alt = 'Personagem Nervoso';
-        mensagemP.textContent = 'Não me faça perder o meu tempo!'; 
-    } 
-    // cenário 2: Usuário digitou uma informação, o personagem muda para alegre
-    else {
-        personagemImg.src = 'imgs/alegre.png';
-        personagemImg.alt = 'Personagem Alegre';
-        mensagemP.textContent = `${nomeUsuario}, seja bem-vindo!`; 
+function estadoAssustado() {
+    setEstado('assustado', 'O que você quer?');
+}
+
+
+function estadoAposClique() {
+
+    const nomeUsuario = prompt("Por favor, digite seu nome:");
+
+    if (nomeUsuario === null || nomeUsuario.trim() === "") {
+        setEstado('nervoso', 'Não me faça perder o meu tempo!', 'imagem-nervoso');
     }
-});
+
+    else {
+        setEstado('alegre', `${nomeUsuario}, seja bem-vindo!`, 'imagem-alegre');
+    }
+} -
+
+    estadoPensativo();
+
+personagem.addEventListener('mouseover', estadoAssustado);
+personagem.addEventListener('click', estadoAposClique);
+personagem.addEventListener('mouseout', estadoPensativo);
